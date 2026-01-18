5d8010dfc5cc3a7ad7e429f43dc9bb9cfb1b38ba
atp-aung <atp.aung.5555@gmail.com>
2026-01-19 00:38:37 +0630

    c

--- lib/main.dart
+++ lib/main.dart
@@ -26,43 +26,36 @@ class Counter extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Counter> createState() => _CounterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CounterState extends State<Counter> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+              body: Column(
+          children: [
+            Text("$_counter"),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                setState(() => _counter++);
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+//  auto-formatting nicer for build methods.
     );
   }
 }
