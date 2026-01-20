f7d2e6ffa4faf4a36b2a7ab4f3c3eca91c48bee7
atp-aung <atp.aung.5555@gmail.com>
2026-01-20 21:32:27 +0630

    b

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         body: ListView.builder(
           itemCount: myList.length,
           itemBuilder: (context, index) {
-            return Text("m${myList[index]}");
+            return Text("${myList[index]}");
           },
         ),
       ),
