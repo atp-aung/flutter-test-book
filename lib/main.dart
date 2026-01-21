20d9b30759aa55680c86ec217e5dd336154de206
atp-aung <atp.aung.5555@gmail.com>
2026-01-21 19:00:15 +0630

    c

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,83 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TextFieldDemo(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  bool _isDarkMode = false;
+class TextFieldDemo extends StatefulWidget {
+  const TextFieldDemo({super.key});
+
+  @override
+  State<TextFieldDemo> createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  String _info = "";
+
+  @override
+  void dispose() {
+    _name.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TextField Demo'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Theme Switch Demo'),
-        ),
-        body: Center(
-          child: SwitchListTile(
-            title: const Text('Dark Mode'),
-            value: _isDarkMode,
-            onChanged: (bool newValue) {
-              setState(() {
-                _isDarkMode = newValue;
-              });
-            },
-            activeColor: Colors.blue,
-            secondary: const Icon(Icons.lightbulb),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _name,
+              decoration: const InputDecoration(
+                labelText: "Name",
+                hintText: "Enter name",
+                suffixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+                hintText: "Enter password",
+                suffixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _info = "Name : ${_name.text}, Password : ${_password.text}";
+                });
+              },
+              child: const Text("Save"),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _info,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
     );
