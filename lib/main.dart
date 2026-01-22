f456680209f65e0d8ed818b8e142cbf596adffb5
atp-aung <atp.aung.5555@gmail.com>
2026-01-22 19:38:30 +0630

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/profile': (context) => ProfileScreen(),
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to Profile Page'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/profile');
-          },
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ContainerDemo(),
     );
   }
 }
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ContainerDemo extends StatelessWidget {
+  const ContainerDemo({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile Page"),
-      ),
-      body: Center(
-        child: Text("Welcome to the Profile Page!"),
+      appBar: AppBar(title: const Text('Container Widget Demo')),
+      body: Container(
+        height: 80,
+        width: 260,
+        color: Colors.blueGrey,
+        alignment: Alignment.topLeft,
+        margin: const EdgeInsets.only(top: 100),
+        transform: Matrix4.rotationZ(-0.25),
+        child: const Text(
+          "kyae",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
