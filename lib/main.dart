c299e66d4eb5af1a2c6fe8c5cf4a20db3ef4f620
atp-aung <atp.aung.5555@gmail.com>
2026-02-05 13:00:14 +0630

    x

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class ContainerDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('image Demo')),
-      body: Image.asset('assets/lisbon.jpg'),
+      body: Image.asset('assets/loddg.png'),
       // body: Image.network(
       //     'https://cdn.pixabay.com/photo/2023/09/25/20/38/lisbon-8275994_1280.jpg'),
     );
