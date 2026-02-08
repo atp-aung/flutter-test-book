522edb5253427498eb8dbf853efae934e5887859
atp-aung <atp.aung.5555@gmail.com>
2026-02-08 13:59:41 +0630

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,16 @@ class ContainerDemo extends StatelessWidget {
           children: [
             const Center(child: CircularProgressIndicator()),
             Center(
-              // child: FadeInImage.assetNetwork(
-              //     placeholder: String.fromCharCode(0xe3b0),
-              //     image:
-              //         'https://cdn.pixabay.com/photo/2020/03/25/12/41/beach-4967176_1280.jpg'),
-              child: FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image:
-                    'https://cdn.pixabay.com/photo/2020/03/25/12/41/beach-4967176_1280.jpg',
-              ),
+              child: FadeInImage.assetNetwork(
+                  //placeholder: String.fromCharCode(0xe3b0),
+                  placeholder: '../bbb/hhh.jpg',
+                  image:
+                      'https://cdn.pixabay.com/photo/2020/03/25/12/41/beach-4967176_1280.jpg'),
+              // child: FadeInImage.memoryNetwork(
+              //   placeholder: kTransparentImage,
+              //   image:
+              //       'https://cdn.pixabay.com/photo/2020/03/25/12/41/beach-4967176_1280.jpg',
+              // ),
             ),
           ],
         ),
